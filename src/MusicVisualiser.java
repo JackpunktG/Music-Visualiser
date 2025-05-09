a63ee1372c753fa60d8d1c6fdd07f8b4cd51cbd2
Jack B <jack.goldsbrough@outlook.com>
2025-05-09 16:48:49 +0200

    Options for rectangle or circle mode added

--- src/MusicVisualiser.java
+++ src/MusicVisualiser.java
@@ -1,6 +1,4 @@
 import javax.sound.sampled.*;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
 import java.io.File;
 import java.io.IOException;
 import javax.swing.*;
@@ -14,7 +12,7 @@ public class MusicVisualiser
 {
     public static void main(String[] args)
     {
-
+        int options = Integer.parseInt(JOptionPane.showInputDialog("Input '1' for Rectangle mode and '2' for Circle mode :)"));
         int frameW = Integer.parseInt(JOptionPane.showInputDialog("Gimme the size ya window - Width: "));
         int frameH = Integer.parseInt(JOptionPane.showInputDialog("Gimme the size ya window - Height: "));
         JFrame frame = new JFrame("Visualise");                         //opening frame
@@ -25,102 +23,130 @@ public class MusicVisualiser
         JPanel panel = new JPanel();
         panel.setLayout(null);
         panel.setBackground(new Color(148, 50, 230));
-/*
-        //RECTANLGE
-        int recTotal = Integer.parseInt(JOptionPane.showInputDialog("Resolution of wave?? Best either to do your Window width for high fidelity\n or between 10 - 100 for blocker graphics\n\n  ride the waves... ;)"));
-        rectangle[] Rectangle = new rectangle[recTotal];   //declaring the rectanlge array with custom amount
 
-        for (int i = 0; i < recTotal; i++) {
-            Rectangle[i] = new rectangle((frame.getWidth() / recTotal) * i, frame.getHeight(), frame.getWidth() / recTotal, 20);  //iterate through initalising them with custom values
-            panel.add(Rectangle[i]);                //adding straight to frame - visuals
-        }
+        switch (options) {
+            case 1: 
+            {           //RECTANGLE MODE
+                int recTotal = Integer.parseInt(JOptionPane.showInputDialog("Resolution of wave?? Best either to do your Window width for high fidelity\n or between 10 - 100 for blocker graphics\n\n  ride the waves... ;)"));
+                rectangle[] Rectangle = new rectangle[recTotal];   //declaring the rectanlge array with custom amount
 
-        frame.add(panel);
-        frame.setVisible(true);
-        rectangle.screenheight = frame.getHeight();  //set screen height for wave calculations
+                for (int i = 0; i < recTotal; i++) {
+                    Rectangle[i] = new rectangle((frame.getWidth() / recTotal) * i, frame.getHeight(), frame.getWidth() / recTotal, 20);  //iterate through initalising them with custom values
+                    panel.add(Rectangle[i]);                //adding straight to frame - visuals
+                }
+                frame.add(panel);
+                frame.setVisible(true);
+                rectangle.screenheight = frame.getHeight();  //set screen height for wave calculations
 
- */
-        //CIRCLE
-        int circleTotal = 1444;
-        Circle[] Circle = new Circle[circleTotal];
-        int colAndRow = (int)Math.sqrt(circleTotal);
-        System.out.println(colAndRow);
-        int circleIndex = 0;
+                JFileChooser fileChooser = new JFileChooser();                  //file selector
+                int result = fileChooser.showOpenDialog(null);
+                File selectedFile = null;
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    selectedFile = fileChooser.getSelectedFile();
+                }
 
-        for (int row = 0; row < colAndRow; row++) {
-            for (int col = 0; col < colAndRow; col++) {
-                Circle[circleIndex] = new Circle(row, col, BLUE);
-                panel.add(Circle[circleIndex]);
-                circleIndex++;
+
+
+                try {
+                    File soundFile = new File(selectedFile.getAbsolutePath());
+                    AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundFile);      //start streams file
+                    AudioFormat format = audioStream.getFormat();                               //gets format from audio
+                    DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+                    float sampleRate = format.getSampleRate();
+
+                    SourceDataLine speakers = (SourceDataLine) AudioSystem.getLine(info);       //speaker connection
+                    speakers.open(format);
+                    speakers.start();
+
+                    byte[] buffer = new byte[4096];             //buffered audio stream
+                    int readBuffer;
+
+                    while ((readBuffer = audioStream.read(buffer, 0, buffer.length)) != -1) {
+
+                        for (int i = 0; i < recTotal; i++) {
+                            Rectangle[i].dynamicHeightRectangle(buffer);
+                        }
+                        speakers.write(buffer, 0, readBuffer);
+                    }
+
+                    speakers.drain();
+                    speakers.close();
+                    audioStream.close();
+
+                }
+                catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
+                    e.printStackTrace();
+                }
+                break;
             }
-        }
-       /* panel.addComponentListener(new ComponentAdapter() {
+            case 2: 
+            {   //CIRCLE MDOE
+                int circleTotal = 1444;
+                Circle[] Circle = new Circle[circleTotal];
+                int colAndRow = (int)Math.sqrt(circleTotal);
+                int circleIndex = 0;
 
+                for (int row = 0; row < colAndRow; row++) {
+                    for (int col = 0; col < colAndRow; col++) {
+                        Circle[circleIndex] = new Circle(row, col, BLUE);
+                        panel.add(Circle[circleIndex]);
+                        circleIndex++;
+                    }
+                }
 
-            public void componentResized(ComponentEvent e) {
+                frame.add(panel);
+                frame.setVisible(true);
+
                 for (int i = 0; i < circleTotal; i++) {
-                    Circle[i].updateBounds(3, 3, panel.getWidth(), panel.getHeight(), 10);
-                }
-            }
-        });
-  */
-        frame.add(panel);
-        frame.setVisible(true);
-
-        for (int i = 0; i < circleTotal; i++) {
-            Circle[i].updateBounds(colAndRow, colAndRow, panel.getWidth(), panel.getHeight(), 0);
-        }
-
-        JFileChooser fileChooser = new JFileChooser();                  //file selector
-        int result = fileChooser.showOpenDialog(null);
-        File selectedFile = null;
-        if (result == JFileChooser.APPROVE_OPTION) {
-            selectedFile = fileChooser.getSelectedFile();
-
-        }
-
-        try {
-            File soundFile = new File(selectedFile.getAbsolutePath());
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundFile);      //start streams file
-            AudioFormat format = audioStream.getFormat();                               //gets format from audio
-            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-            float sampleRate = format.getSampleRate();
-
-            SourceDataLine speakers = (SourceDataLine) AudioSystem.getLine(info);       //speaker connection
-            speakers.open(format);
-            speakers.start();
-
-            byte[] buffer = new byte[4096];             //buffered audio stream
-            int readBuffer;
-
-
-
-
-
-            while ((readBuffer = audioStream.read(buffer, 0, buffer.length)) != -1) {
-
-               /* for (int i = 0; i < recTotal; i++) {
-                    Rectangle[i].dynamicHeightRectangle(buffer);
+                    Circle[i].updateBounds(colAndRow, colAndRow, panel.getWidth(), panel.getHeight(), 0);
                 }
 
-
-                */
-                for (int i = 0; i < circleTotal; i++) {
-                    Circle[i].dynamicBounds(buffer);
+                JFileChooser fileChooser = new JFileChooser();                  //file selector
+                int result = fileChooser.showOpenDialog(null);
+                File selectedFile = null;
+                if (result == JFileChooser.APPROVE_OPTION) {
+                    selectedFile = fileChooser.getSelectedFile();
 
                 }
-                speakers.write(buffer, 0, readBuffer);
 
+                try {
+                    File soundFile = new File(selectedFile.getAbsolutePath());
+                    AudioInputStream audioStream = AudioSystem.getAudioInputStream(soundFile);      //start streams file
+                    AudioFormat format = audioStream.getFormat();                               //gets format from audio
+                    DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+                    float sampleRate = format.getSampleRate();
+
+                    SourceDataLine speakers = (SourceDataLine) AudioSystem.getLine(info);       //speaker connection
+                    speakers.open(format);
+                    speakers.start();
+
+                    byte[] buffer = new byte[4096];             //buffered audio stream
+                    int readBuffer;
+
+                    while ((readBuffer = audioStream.read(buffer, 0, buffer.length)) != -1) {
+
+                        for (int i = 0; i < circleTotal; i++) {
+                            Circle[i].dynamicBounds(buffer);
+                        }
+                        speakers.write(buffer, 0, readBuffer);
+                    }
+
+                    speakers.drain();
+                    speakers.close();
+                    audioStream.close();
+
+                }
+                catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
+                    e.printStackTrace();
+                }
+                break;
             }
-
-            speakers.drain();
-            speakers.close();
-            audioStream.close();
-
+            default: {
+                System.out.println("Invalid Option");
+                break;
             }
-        catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
-            e.printStackTrace();
         }
+
 
 
     }
